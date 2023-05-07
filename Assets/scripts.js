27a1dc39d0d50b0c6c0bf11b9b3843bf0ea2cd1b
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-05-07 17:56:37 -0400

    initial push of html, basic css and script instructions

---
+++ Assets/scripts.js
@@ -0,0 +1,19 @@
+//multiple choice questions - add ol and li elements
+//associate questions and answers
+//one has to be the correct answer
+//timer
+//dynamically updated HTML and CSS via js
+//store high scores
+//display high scores
+//start button - timer kicks off
+//start button - presented with a question
+//response to question triggers - correct or incorrect on bottom display
+//response to question - triggers points
+//response to question - triggers another question
+//response to question - incorrect answer subtracts time clock
+//game over - when all questions answered OR
+//game over - when timer reaches 0
+//on game over - save initials and score
+//hover effect on button
+
+
