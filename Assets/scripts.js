a8431c4b33274f3fb2eac2539a8b39c4bc18f483
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-05-09 00:15:17 -0400

    working buttons with answers and score

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -24,33 +24,33 @@ var questionGroup = document.getElementById('questiongroup');
 var questionLiteral = document.getElementById('question');
 
 var questionsPool = [];
-var questionsUsed = [[{"question": "Question 1", "text": "answer1","data-result":false},
-                 {"question": "Question 1", "text": "Q1answer2","data-result":false},
-                 {"question": "Question 1", "text": "Q1answer3","data-result":true},
-                 {"question": "Question 1", "text": "Q1answer4","data-result":false}
-                ],
-                [{"question": "Question 2", "text": "Q2answer1","data-result":false},
-                 {"question": "Question 2", "text": "Q2answer2","data-result":false},
-                 {"question": "Question 2", "text": "Q2answer3","data-result":true},
-                 {"question": "Question 2", "text": "Q2answer4","data-result":false}
-                ],
-                [{"question": "Question 3", "text": "Q3answer1","data-result":false},
-                 {"question": "Question 3", "text": "Q3answer2","data-result":false},
-                 {"question": "Question 3", "text": "Q3answer3","data-result":true},
-                 {"question": "Question 3", "text": "Q3answer4","data-result":false}
-               ],
-               [{"question": "Question 4", "text": "Q3answer1","data-result":false},
-                {"question": "Question 4", "text": "Q3answer2","data-result":false},
-                {"question": "Question 4", "text": "Q3answer3","data-result":true},
-                {"question": "Question 4", "text": "Q3answer4","data-result":false}
-                ]];
+var questionsUsed = [[{ "question": "Question 1", "text": "Q1answer1", "result": false },
+{ "question": "Question 1", "text": "Q1answer2", "result": false },
+{ "question": "Question 1", "text": "Q1answer3", "result": true },
+{ "question": "Question 1", "text": "Q1answer4", "result": false }
+],
+[{ "question": "Question 2", "text": "Q2answer1", "result": false },
+{ "question": "Question 2", "text": "Q2answer2", "result": false },
+{ "question": "Question 2", "text": "Q2answer3", "result": true },
+{ "question": "Question 2", "text": "Q2answer4", "result": false }
+],
+[{ "question": "Question 3", "text": "Q3answer1", "result": false },
+{ "question": "Question 3", "text": "Q3answer2", "result": false },
+{ "question": "Question 3", "text": "Q3answer3", "result": true },
+{ "question": "Question 3", "text": "Q3answer4", "result": false }
+],
+[{ "question": "Question 4", "text": "Q4answer1", "result": false },
+{ "question": "Question 4", "text": "Q4answer2", "result": false },
+{ "question": "Question 4", "text": "Q4answer3", "result": true },
+{ "question": "Question 4", "text": "Q4answer4", "result": false }
+]];
 
 function countdown() {
     var timeLeft = 78;
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timerBox.textContent = "Time Left " + timeLeft;
-        if(timeLeft < 0) {
+        if (timeLeft < 0) {
             clearInterval(timeInterval);
             displayTimeUp();
         }
@@ -74,36 +74,55 @@ function startQuiz(event) {
 }
 
 function viewHighScore(event) {
-//    display highscore from storage
+    //    display highscore from storage
 }
 
-function makeSelection() {
-  console.log("pink");
-  return;
+function makeSelection(event) {
+    var clickedAnswer = event.target;
+    var result = clickedAnswer.getAttribute("data-result");
+    console.log(result);
+    if (result === "true") {
+        newScore = 10;
+    } else {
+        newScore = -10;
+    }
+    console.log(newScore);
+    saveScores(newScore);
+    console.log("pink");
 }
 
 
 
 function displayQuestion() {
-var listElement = document.createElement("ol");
-questionGroup.appendChild(listElement);
-var questionIndex = Math.floor(Math.random() * questionsPool.length);
-console.log(questionIndex);
-questionLiteral.textContent = questionsPool[questionIndex][0].question;
-console.log(questionsPool[questionIndex][0].text)
-console.log(questionsPool[questionIndex][1].text);
-console.log(questionsPool[questionIndex].length);
-for (i = 0; i < questionsPool[questionIndex].length;i++) {
-    var liItems = document.createElement("li");
-    questionGroup.appendChild(liItems);
-    liItems.textContent = questionsPool[questionIndex][i].text;
-}
+    var listElement = document.createElement("ol");
+    questionGroup.appendChild(listElement);
+    var questionIndex = Math.floor(Math.random() * questionsPool.length);
+    console.log(questionIndex);
+    questionLiteral.textContent = questionsPool[questionIndex][0].question;
+    console.log(questionsPool[questionIndex][0].text)
+    console.log(questionsPool[questionIndex][1].text);
+    console.log(questionsPool[questionIndex].length);
+    for (i = 0; i < questionsPool[questionIndex].length; i++) {
+        var liItems = document.createElement("li");
+        var liButton = document.createElement("button");
+        listElement.appendChild(liButton);
+        liButton.appendChild(liItems);
+        liItems.textContent = questionsPool[questionIndex][i].text;
+        liItems.dataset.result = questionsPool[questionIndex][i].result;
+    }
 }
 function displayAnswerOpts() {
 
 }
 
+function saveScores(score) {
+    var highScore = localStorage.getItem("high-score");
+    var newHighScore = score + highScore;
+    console.log(newHighScore);
+    //localStorage.setItem("high-score", 0);
+    //localStorage.setItem("initials","")
+}
 
-startButton.addEventListener("click",startQuiz);
+startButton.addEventListener("click", startQuiz);
 highScore.addEventListener("click", viewHighScore);
 questionGroup.addEventListener("click", makeSelection);
